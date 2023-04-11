3811a3aa313131e04f2d0d69beb5a71143655132
Ayush Shrivastava <ayushshrivastava600@gmail.com>
2023-04-12 03:08:59 +0530

    Edited scss file in python file

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+https://free-thesis.com/wp-content/uploads/2022/02/Depression-Detection-Framework-EEG-free-thesis.png",    
+"https://free-thesis.com/wp-content/uploads/2018/04/output-of-PSO-tuned-MPPT-control.png",    
+"https://free-thesis.com/wp-content/uploads/2018/04/PSO-Tuned-MPPT-Control-of-PV-array.png",    
+"https://free-thesis.com/wp-content/uploads/2018/04/2-1.png",    
+"https://free-thesis.com/wp-content/uploads/2018/04/motor_5.png",    
+"https://free-thesis.com/wp-content/uploads/2018/04/motor_2.png
